3f19ed81e417194ea290e2a9ef87f8caf12eb0b8
Geyi Liu <g.liu@alumni.ubc.ca>
2016-06-14 11:21:59 -0700

    code added

---
+++ confusionMatrix.R
@@ -0,0 +1,28 @@
+#Actual conditions of patients
+actualBrain<-rep("brain cancer",8);
+actualColorectal<-rep("colorectal cancer",6);
+actualKidney<-rep("kidney stone",15);
+actualLung<-rep("lung cancer",3);
+
+#Diagnosed conditions of patients
+predictedBrain<-rep("brain cancer",6);
+predictedColrectal<-rep("colorectal cancer",6);
+predictedKidney<-rep("kidney stone",19);
+predictedLung<-rep("lung cancer",1);
+
+#Actual
+Reference<-c(actualBrain,actualColorectal,actualKidney,actualLung);
+#Predicted(i.e diagnosed)
+Predicted<-c(predictedBrain,predictedColrectal,predictedKidney,predictedLung);
+
+#Generate data frame of reference and predicted values
+myDataFrame<-data.frame(Reference,Predicted);
+
+#Create confusion matrix (as seen in most literature)
+myConfusionMatrix<-table(Predicted, Reference);
+print(myConfusionMatrix)
+
+#Create confusion matrix using library caret
+library(caret);
+CaretConfusionMatrix<-as.table(confusionMatrix(Predicted,Reference));
+print(CaretConfusionMatrix)
